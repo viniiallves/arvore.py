d2b1f3fb31931acc2dde0cc92b6e2214b044a17b
viniiallves <74032110+viniiallves@users.noreply.github.com>
2020-11-12 23:35:13 -0300

    Update arvore.py

--- arvore.py
+++ arvore.py
@@ -1,3 +1,11 @@
 import pandas as import pd
-import numpy as 
-from sklearn
+import numpy as np
+from sklearn as tree
+
+train = pd.read.csv ()
+y_train = train ()
+x_train = train.drop ([], axis=1).values
+decision_tree = tree.DecisionTreeClassifier(max_depth=)
+decision_tree.fit (x_train, y_train)
+
+with open (,) as f:
